0eca603c64d90677596f9349b2df8b3bb3bf7d61
David Kamphausen <david.kamphausen@web.de>
2011-11-05 23:14:34 +0100

    mainmenu display working

---
+++ lib/ant_menu_app.cc
@@ -0,0 +1,39 @@
+/*
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) 2011  <copyright holder> <email>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include "ant_menu_app.h"
+#include "ag_layout.h"
+
+AntMenuApp::AntMenuApp():AGApplication()
+{
+
+}
+
+AntMenuApp::~AntMenuApp() throw()
+{
+
+}
+
+
+void AntMenuApp::init()
+{
+  layout=new AGLayout(0);
+  layout->loadXML(loadFile("data/gui/layout/mainmenu.xml"));
+  setMainWidget(layout);
+}
