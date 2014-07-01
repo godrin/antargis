f91906f613d1c58cc59101a4bf88fb401fa87707
David Kamphausen <david.kamphausen@web.de>
2014-07-01 17:23:41 +0200

    some doku

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@
 #include <ant_game_app.h>
 #include <ag_rand.h>
 
+/**
+ * Main entry for the game. main should get wrapped within SDL_main on windows automatically.
+ */ 
 int main ( int argc, char **argv ) {
     setDebugLevel(0);
 
@@ -21,20 +24,24 @@ int main ( int argc, char **argv ) {
     if ( argc>1 ) {
 
       AntGameApp app ( getVideo()->width(),getVideo()->height() );
+      // continue saved game (for testing purpose)
       if(AGString(argv[1])=="c")
         app.init("tempSaved");
       else if(AGString(argv[1])=="x")
         app.init("data/levels/tutorial/tutorial0.antlvl");
       else
+        // run specific level
         app.init(argv[1]);
       app.run();
       return 0;
     }
 
+    // run intro
     AntIntroApp intro;
     intro.init();
     intro.run();
 
+    // run main menu, which then runs other 'apps'
     AntMenuApp menu;
     menu.init();
     menu.run();
